108027828ad5f4a13400aa5ed9b634b9da2d2f9d
Deepanshu Bharaj <deepanshubharaj6@gmail.com>
2026-08-31 22:51:14 +0530

    some changes

--- reverse_a_string.py
+++ reverse_a_string.py
@@ -1,25 +1,29 @@
 # reverse a string
-class solution():
-    def rev_string(self,s:list[str]):
-        i=1
-        print(s,type(s))
-        left,right= 0 , len(s) -1          # respectively 
-        while left<right:
-            s[left],s[right]=s[right],s[left]
-            left+=1
-            right-=1
-            print("swap number ",i)
-            i+=1
-        
-        print("total swaps required are :",i-1)     
-        return s
+class Solution:
+    def rev_string(self, s: str) -> str:
+        # Convert string to list for mutability
+        chars = list(s)
+        left, right = 0, len(chars) - 1
+        swaps = 0
+
+        while left < right:
+            chars[left], chars[right] = chars[right], chars[left]
+            left += 1
+            right -= 1
+            swaps += 1
+            print("swap number", swaps)
+
+        print("total swaps required are:", swaps)
+        return ''.join(chars)
+
+
 
 """
 left and right are two pointers
 Left is 0 and right is len(s) -1 to reach last index value
 """
-obj1=solution()
-s=list(input("enter a string containing vowels"))
+obj1 = Solution()
+s = input("enter a string containing vowels")
 print(obj1.rev_string(s))
 
 """
